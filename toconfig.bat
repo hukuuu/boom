e1ea0a53354f633359b054f679e124b2b19a155e
hukuuu <hukuuu@gmail.com>
2013-09-19 11:28:19 +0300

    added utility scripts ( copy the .boom file from and into the repo )

---
+++ toconfig.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+COPY %USER_HOME%\boom_repo\boom\.boom %USER_HOME%\.boom
+ECHO Copied .boom to the config folder!
+PAUSE
+EXIT /B
---
+++ torepo.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+COPY %USER_HOME%\.boom %USER_HOME%\boom_repo\boom\.boom
+ECHO Copied .boom into the repo folder!
+PAUSE
+EXIT /B
